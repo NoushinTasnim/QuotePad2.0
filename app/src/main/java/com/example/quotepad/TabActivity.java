b68868701b65d2cbc82264edb8e1a104bab5efdc
NoushinTasnim <102020716+NoushinTasnim@users.noreply.github.com>
2022-10-07 10:44:15 +0600

    SignIn Fragment
    Only layout, no functions

--- app/src/main/java/com/example/quotepad/TabActivity.java
+++ app/src/main/java/com/example/quotepad/TabActivity.java
@@ -18,11 +18,60 @@ import java.util.List;
 
 public class TabActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private SignInFragment signInFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        viewPager = findViewById(R.id.sign_view_pager);
+        tabLayout = findViewById(R.id.tab_lay);
+
+        signInFragment = new SignInFragment();
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        TabActivity.ViewPagerAdapter viewPagerAdapter = new TabActivity.ViewPagerAdapter(getSupportFragmentManager(), 0);
+
+        viewPagerAdapter.addFragment(signInFragment, "Sign In");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+        private List<String> fragment_title = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            fragment_title.add(title);
+        }
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragment_title.get(position);
+        }
     }
 }
