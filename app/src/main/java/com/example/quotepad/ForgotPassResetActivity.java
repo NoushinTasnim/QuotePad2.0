7d15ed90d77f069cb9c8078ee849e46248be94a4
Aka5h23 <86167036+Aka5h23@users.noreply.github.com>
2022-10-14 22:07:26 +0600

    Errors Fixed

---
+++ app/src/main/java/com/example/quotepad/ForgotPassResetActivity.java
@@ -0,0 +1,14 @@
+package com.example.quotepad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPassResetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass_reset);
+    }
+}
